a3f5ac80f1352b097cf654e6748df69fe34b2876
IvaTsu <ivan.tsud@gmail.com>
2018-10-07 16:05:33 +0300

    Config Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,1 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
+
+const req = require.context("../src/", true, /.stories.js$/);
+const WelcomeStory = require("./welcomeStory.js");
+
+function loadStories() {
+  WelcomeStory;
+  req.keys().forEach(file => req(file));
+}
+
+configure(loadStories, module);
